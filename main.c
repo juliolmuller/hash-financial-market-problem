0748a96e56e625d3775a50699603f802dc3f62ec
Julio Muller <juliodolago@gmail.com>
2020-09-27 19:20:04 -0300

    Created function to resolve the problem

--- main.c
+++ main.c
@@ -1,11 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "finmkt.h"
+#include "hash.h"
+
+void problema_mercado_financeiro(List* partners, List *portfolios)
+{
+  HashTable *cancelledPartners = hash_create(5);
+  HashTable *activePortfolios = hash_create(5);
+
+  while (partners) {
+    if (portfolios->value) {
+      if (!hash_search(cancelledPartners, partners->value)) {
+        hash_insert(activePortfolios, portfolios->value);
+      }
+    } else {
+      hash_insert(cancelledPartners, partners->value);
+    }
+
+    partners = partners->next;
+    portfolios = portfolios->next;
+  }
+
+  printf("\nParceiros com ordens canceladas:");
+  hash_print(cancelledPartners);
+  printf("\nPortfolios ativos:");
+  hash_print(activePortfolios);
+
+  hash_free(cancelledPartners);
+  hash_free(activePortfolios);
+}
 
 void main()
 {
   char operation, lineBreak;
   int partner, portfolio;
+  long int count = 0;
   List *partnersList = NULL;
   List *portfoliosList = NULL;
 
@@ -14,16 +42,13 @@ void main()
     scanf("%c%d%d%c", &operation, &partner, &portfolio, &lineBreak);
     partnersList = list_add(partnersList, partner);
     portfoliosList = list_add(portfoliosList, portfolio);
+    count++;
   } while (partner);
 
-  if (portfoliosList) {
-    partnersList = partnersList->next;
-    portfoliosList = portfoliosList->next;
-    while (partnersList) {
-      operation = portfoliosList->value ? '+' : '-';
-      printf("%c %5d %7d \n", operation, partnersList->value, portfoliosList->value);
-      partnersList = partnersList->next;
-      portfoliosList = portfoliosList->next;
-    }
-  }
+  partnersList = partnersList->next;
+  portfoliosList = portfoliosList->next;
+  problema_mercado_financeiro(partnersList, portfoliosList);
+
+  list_clear(partnersList);
+  list_clear(portfoliosList);
 }
